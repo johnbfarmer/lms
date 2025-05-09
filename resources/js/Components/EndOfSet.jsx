6ba5d5edf32269f0117972e137533213bdd8118b
jfarmer <jfarmer@acronym.com>
2025-05-09 09:07:08 -0600

    20250509-1

---
+++ resources/js/Components/EndOfSet.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Modal from '@/Components/Modal';
+
+export default function EndOfSet(props) {
+    return (
+        <div className="mx-auto my-6 max-w-7xl space-y-6 sm:px-6 lg:px-8">
+            <Modal show={props.show} onClose={props.onClose} >
+                <div className={`p-12 shadow sm:rounded-lg`}>
+                    <div className="">
+                        that's all
+                    </div>
+                </div>
+            </Modal>
+
+        </div>
+    );
+}
